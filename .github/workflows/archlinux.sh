d60d687b40a7cdae6793666b40a4ce6610da3dc2
mbkma <johannes.unruh@fau.de>
2026-01-27 23:12:50 +0100

    ci: migrate from Travis CI to GitHub Actions
    Replace Travis CI configuration with GitHub Actions workflows.
    Add CI build workflow for Debian, Fedora, Ubuntu, and Archlinux.
    Add release workflow for automated GitHub releases on tags.
    Add dependabot configuration for GHA pin updates.
    Remove obsolete .travis.yml.

---
+++ .github/workflows/archlinux.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+set -eo pipefail
+
+# Use grouped output messages
+infobegin() {
+	echo "::group::${1}"
+}
+infoend() {
+	echo "::endgroup::"
+}
+
+# Required packages on Archlinux
+requires=(
+	ccache # Use ccache to speed up build
+)
+
+requires+=(
+	gcc
+	git
+	make
+	autoconf-archive
+	intltool
+	mate-common
+	which
+)
+
+infobegin "Update system"
+pacman --noconfirm -Syu
+infoend
+
+infobegin "Install dependency packages"
+pacman --noconfirm -S ${requires[@]}
+infoend
---
+++ .github/workflows/debian.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+
+set -eo pipefail
+
+# Use grouped output messages
+infobegin() {
+	echo "::group::${1}"
+}
+infoend() {
+	echo "::endgroup::"
+}
+
+# Required packages on Debian
+requires=(
+	ccache # Use ccache to speed up build
+)
+
+requires+=(
+	git
+	icon-naming-utils
+	intltool
+	libgtk2.0-dev
+	libxml-parser-perl
+	make
+	mate-common
+	pkg-config
+)
+
+infobegin "Update system"
+apt-get update -qq
+infoend
+
+infobegin "Install dependency packages"
+env DEBIAN_FRONTEND=noninteractive \
+	apt-get install --assume-yes \
+	${requires[@]}
+infoend
